cebf56432ec354dd3a13e3cb397222e4ebcbe3f3
Vimal Sheth <vimal.sheth@gmail.com>
2015-11-22 23:45:31 +0530

    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,26 +1,43 @@
+# makeCacheMatrix stores a matrix X in memory
+# m is supposed to be inverse of the matrix, to be retrieved through getinverse() function
+
 
 makeCacheMatrix <- function(x = matrix()) {
+  
   m<-NULL
-  set<-function(y){
-  x<<-y
-  m<<-NULL
-}
-get<-function() x
-setmatrix<-function(solve) m<<- solve
-getmatrix<-function() m
-list(set=set, get=get,
-   setmatrix=setmatrix,
-   getmatrix=getmatrix)
+  
+  set<-function(y)
+  {
+    x<<-y
+    m<<-NULL
+  }
+  
+  get<-function() x
+  
+  setinverse<-function(solve) m<<- solve
+  
+  getinverse<-function() m
+  
+  list(set=set, get=get,
+       setinverse=setinverse,
+       getinverse=getinverse)
 }
 
 cacheSolve <- function(x=matrix(), ...) {
-    m<-x$getmatrix()
-    if(!is.null(m)){
-      message("getting cached data")
-      return(m)
-    }
-    datos<-x$get()
-    m<-solve(datos, ...)
-    x$setmatrix(m)
-    m
+  
+  m<-x$getinverse()
+  
+  if(!is.null(m))
+  {
+    message("getting cached data")
+    return(m)
+  }
+  
+  datos<-x$get()
+  
+  m<-solve(datos, ...)
+  
+  x$setinverse(m)
+  
+  m
 }
